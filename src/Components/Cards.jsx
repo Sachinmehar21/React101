a5a7f587563a47ab75b031e4397184de9bcc1b1e
Sachinmehar21 <130161660+Sachinmehar21@users.noreply.github.com>
2024-05-04 01:23:55 +0530

    Components
    to create, import/export components

---
+++ src/Components/Cards.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cards = () => {
+  return (
+    <>
+      <div className="text-red-700 h-2vw w-1/5 bg-red-500">abc</div>
+    </>
+  );
+};
+
+export default Cards;
